b3acd8e2ea8ad748929b158a84a7a9632f110b19
Christian Fobel <christian@fobel.net>
2016-07-28 09:24:33 -0400

    Fix #2: add space before unit prefix
    - Use [SI convention][1] is for a space between the quantity and the SI
      unit prefix.  Note that this commit breaks API compatibility with
      existing code.
    
     - Update test cases to match new format.
    
     - Add `format_str` keyword argument to `si_format` function, with
       default of `'{value} {prefix}'` (note the space between `{value}` and
       `{prefix}`).
    
    See [#2][2] for more details.
    
    [1]: http://physics.nist.gov/cuu/Units/checklist.html
    [2]: https://github.com/cfobel/si-prefix/issues/2

--- si_prefix/__init__.py
+++ si_prefix/__init__.py
@@ -71,57 +71,85 @@ def prefix(expof10):
     return SI_PREFIX_UNITS[si_level + prefix_levels]
 
 
-def si_format(value, precision=1):
-    # Format value to string with SI prefix, using the specified precision.
-    #
-    # For example, with `precision=2`:
-    #
-    #     1e-27 --> 1.00e-27
-    #     1.764e-24 --> 1.76 y
-    #     7.4088e-23 --> 74.09 y
-    #     3.1117e-21 --> 3.11 z
-    #     1.30691e-19 --> 130.69 z
-    #     5.48903e-18 --> 5.49 a
-    #     2.30539e-16 --> 230.54 a
-    #     9.68265e-15 --> 9.68 f
-    #     4.06671e-13 --> 406.67 f
-    #     1.70802e-11 --> 17.08 p
-    #     7.17368e-10 --> 717.37 p
-    #     3.01295e-08 --> 30.13 n
-    #     1.26544e-06 --> 1.27 u
-    #     5.31484e-05 --> 53.15 u
-    #     0.00223223 --> 2.23 m
-    #     0.0937537 --> 93.75 m
-    #     3.93766 --> 3.94
-    #     165.382 --> 165.38
-    #     6946.03 --> 6.95 k
-    #     291733 --> 291.73 k
-    #     1.22528e+07 --> 12.25 M
-    #     5.14617e+08 --> 514.62 M
-    #     2.16139e+10 --> 21.61 G
-    #     9.07785e+11 --> 907.78 G
-    #     3.8127e+13 --> 38.13 T
-    #     1.60133e+15 --> 1.60 P
-    #     6.7256e+16 --> 67.26 P
-    #     2.82475e+18 --> 2.82 E
-    #     1.1864e+20 --> 118.64 E
-    #     4.98286e+21 --> 4.98 Z
-    #     2.0928e+23 --> 209.28 Z
-    #     8.78977e+24 --> 8.79 Y
-    #     3.6917e+26 --> 369.17 Y
-    #     1.55051e+28 --> 15.51e+27
-    #     6.51216e+29 --> 651.22e+27
+def si_format(value, precision=1, format_str='{value} {prefix}',
+              exp_format_str='{value}e{expof10}'):
+    '''
+    Format value to string with SI prefix, using the specified precision.
+
+    Args
+    ----
+
+        value (int, float) : Input value.
+        precision (int) : Number of digits after decimal place to include.
+        format_str (str) : Format string where `{prefix}` and `{value}`
+            represent the SI prefix and the value (scaled according to the
+            prefix), respectively.  The default format matches the format specified
+            [here][1].
+        exp_str : Format string where `{expof10}` and `{value}`
+            represent the exponent of 10 and the value (scaled according to the
+            exponent of 10), respectively.  This format is used if the absolute
+            exponent of 10 value is greater than 24.
+
+    Returns
+    -------
+
+        (str) : `value` formatted according to the [SI prefix style][1].
+
+    Examples
+    --------
+
+    For example, with `precision=2`:
+
+        1e-27 --> 1.00e-27
+        1.764e-24 --> 1.76 y
+        7.4088e-23 --> 74.09 y
+        3.1117e-21 --> 3.11 z
+        1.30691e-19 --> 130.69 z
+        5.48903e-18 --> 5.49 a
+        2.30539e-16 --> 230.54 a
+        9.68265e-15 --> 9.68 f
+        4.06671e-13 --> 406.67 f
+        1.70802e-11 --> 17.08 p
+        7.17368e-10 --> 717.37 p
+        3.01295e-08 --> 30.13 n
+        1.26544e-06 --> 1.27 u
+        5.31484e-05 --> 53.15 u
+        0.00223223 --> 2.23 m
+        0.0937537 --> 93.75 m
+        3.93766 --> 3.94
+        165.382 --> 165.38
+        6946.03 --> 6.95 k
+        291733 --> 291.73 k
+        1.22528e+07 --> 12.25 M
+        5.14617e+08 --> 514.62 M
+        2.16139e+10 --> 21.61 G
+        9.07785e+11 --> 907.78 G
+        3.8127e+13 --> 38.13 T
+        1.60133e+15 --> 1.60 P
+        6.7256e+16 --> 67.26 P
+        2.82475e+18 --> 2.82 E
+        1.1864e+20 --> 118.64 E
+        4.98286e+21 --> 4.98 Z
+        2.0928e+23 --> 209.28 Z
+        8.78977e+24 --> 8.79 Y
+        3.6917e+26 --> 369.17 Y
+        1.55051e+28 --> 15.51e+27
+        6.51216e+29 --> 651.22e+27
+
+    [1]: http://physics.nist.gov/cuu/Units/checklist.html
+    '''
     svalue, expof10 = split(value, precision)
     value_format = '%%.%df' % precision
     value_str = value_format % svalue
-
     try:
-        return '%s%s' % (value_str, prefix(expof10))
+        return format_str.format(value=value_str,
+                                 prefix=prefix(expof10).strip())
     except ValueError:
         sign = ''
         if expof10 > 0:
             sign = "+"
-        return '%se%s%s' % (value_str, sign, expof10)
+        return exp_format_str.format(value=value_str,
+                                     expof10=''.join([sign, str(expof10)]))
 
 
 def si_parse(value):
