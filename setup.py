c70f84702abba38f45588d41bfa01a3ff2e768c0
Christian Fobel <christian@fobel.net>
2018-01-25 13:00:22 -0500

    fix(setup): fix package version lookup

--- setup.py
+++ setup.py
@@ -4,13 +4,11 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 from setuptools import setup
-import sys
 
-sys.path.insert(0, '.')
-import version
+import si_prefix
 
 setup(name='si-prefix',
-      version=version.getVersion(),
+      version=si_prefix.__version__,
       description='Functions for formatting numbers according to SI standards.',
       keywords='si prefix format number precision',
       author='Christian Fobel',
